ab794c120de3214c2305c976867a10bfdcfda637
Jan Bańczerowski <jasiuu2605@gmail.com>
2023-04-19 16:58:34 +0200

    adding listeners functionality on buttons

--- js/Cell.js
+++ js/Cell.js
@@ -1,15 +1,32 @@
+import { UI } from './UI.js';
 
+export class Cell extends UI {
+  constructor(x, y) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.value = 0;
+    this.isMine = false;
+    this.isReveal = false;
+    this.isFlagged = false;
 
-export class Cell {
-    constructor(x,y) {
-        this.x = x;
-        this.y = y;
-        this.value = 0;
-        this.isMine = false;
-        this.isReveal = false;
-        this.isFlagged = false;
+    this.selector = `[data-x= "${this.x}"][data-y="${this.y}"]`;
+    this.element = null;
+  }
 
-        this.selector = `[data-x= "${this.x}"][data-y="${this.y}"]`
-        this.element = null;
-    }
+  revealCell() {
+    this.isReveal = true;
+    this.element.classList.remove("border--concave");
+    this.element.classList.add("border--revealed");
+  }
+
+  createElement() {
+    const element = `<div class=" cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
+    return element;
+  }
+
+  toggleflag() {
+    this.isFlagged = !this.isFlagged;
+    this.element.classList.toggle("cell--is-flag");
+  }
 }
--- js/Game.js
+++ js/Game.js
@@ -1,7 +1,7 @@
-import { Cell } from "./Cell";
+import { Cell } from "./Cell.js";
+import { UI } from "./UI.js";
 
-
-class Game {
+class Game extends UI {
   #config = {
     easy: {
       rows: 8,
@@ -24,9 +24,13 @@ class Game {
   #numberOfCols = null;
   #numberOfMines = null;
 
-  #cells = []
+  #cells = [];
+  #cellsElements = null;
+
+  #board = null;
 
   initializeGame() {
+    this.#handleElements();
     this.#newGame();
   }
 
@@ -39,18 +43,71 @@ class Game {
     this.#numberOfCols = cols;
     this.#numberOfMines = mines;
 
-    this.#gemerateCells();
+    this.#setStyles();
+
+    this.#generateCells();
+    this.#renderBoard();
+
+    this.#cellsElements = this.getElements(this.UiSelectors.cell);
+
+    this.#addCellsEventListeners();
+  }
+
+  #addCellsEventListeners() {
+    this.#cellsElements.forEach((element) => {
+      element.addEventListener("click", this.#handleCellClick);
+      element.addEventListener("contextmenu", this.#handleCellContextMenu);
+    });
+  }
+
+  #handleElements() {
+    this.#board = this.getElement(this.UiSelectors.board);
   }
 
   #generateCells() {
+    this.#cells.length = 0;
     for (let row = 0; row < this.#numberOfRows; row++) {
-        this.cells[row] = []
-        for (let col = 0; col < this.#numberOfCols; col++) {
-            this.#cells[row].push(new Cell(col, row))
-            
-        }
-        
+      this.#cells[row] = [];
+      for (let col = 0; col < this.#numberOfCols; col++) {
+        this.#cells[row].push(new Cell(col, row));
+      }
     }
+  }
+
+  #renderBoard() {
+    this.#cells.flat().forEach((cell) => {
+      this.#board.insertAdjacentHTML("beforeend", cell.createElement());
+      cell.element = cell.getElement(cell.selector);
+    });
+  }
+
+  #handleCellClick = (e) => {
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    this.#cells[rowIndex][colIndex].revealCell();
+  };
+
+  #handleCellContextMenu = (e) => {
+    e.preventDefault()
+    const target = e.target;
+    const rowIndex = parseInt(target.getAttribute("data-y"), 10);
+    const colIndex = parseInt(target.getAttribute("data-x"), 10);
+
+    const cell = this.#cells[rowIndex][colIndex];
+    
+    if (cell.isReveal) {
+        return
+    }
+    cell.toggleflag();
+  };
+
+  #setStyles() {
+    document.documentElement.style.setProperty(
+      "--cells-in-row",
+      this.#numberOfCols
+    );
   }
 }
 
